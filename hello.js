2bfc98b4cef50998110fcbd770a26f22982d08f5
Anahit-Khachatryan <44209605+Anahit-Khachatryan@users.noreply.github.com>
2021-03-27 23:02:57 +0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,16 @@
+export function a() {
+    console.log('bobobo')
+}
+
+export class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+    wo() {
+        console.log(this.name)
+    }
+}
+
+export default function b() {
+    console.log('ddddd')
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import {a as P, Dog} from './hello.js'
+import hopar from './hello.js'
+P();
+
+let b = new Dog('na');
+b.wo();
+hopar();
